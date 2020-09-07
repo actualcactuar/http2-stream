ab60a927ae5e28cd6eac5fb10d715f9d0a143a1c
jiri <kiviniemi.jiri@gmail.com>
2020-09-07 23:18:06 +0300

    handler for streams

--- server/modules/app.js
+++ server/modules/app.js
@@ -2,6 +2,7 @@
 const static = require('./static')
 
 const routes = new Map();
+const streams = new Map();
 
 const buildRouteRegex = route => {
     const formattedRoute = route
@@ -35,6 +36,17 @@ function app(req, res) {
         ':method': requestMethod,
     } } = req;
 
+
+
+    for (const [regex, { route, callback }] of streams.entries()) {
+
+        if (regex.test(requestPath)) {
+            res.writeContinue()
+            return;
+        }
+
+    }
+
     for (const [regex, { route, callback }] of routes.entries()) {
 
         if (regex.test(requestPath)) {
@@ -66,4 +78,25 @@ app.use = (route, callback) => {
     routes.set(regex, { callback, route })
 }
 
-module.exports = app
+const streamer = (stream, headers) => {
+    const {
+        ':path': requestPath,
+    } = headers;
+
+    for (const [regex, { route, callback }] of streams.entries()) {
+
+        if (regex.test(requestPath)) {
+            const params = buildRouteParams(route, requestPath);
+            callback.call(null, stream, { ...headers, params })
+            return;
+        }
+
+    }
+}
+
+streamer.use = (route, callback) => {
+    const regex = buildRouteRegex(route);
+    streams.set(regex, { callback, route })
+}
+
+module.exports = { app, streamer }
--- server/server.js
+++ server/server.js
@@ -1,16 +1,19 @@
 const http2 = require('http2');
 const fs = require('fs');
 
-const app = require('./modules/app')
+const { app, streamer } = require('./modules/app');
 
-app.use('/node/:hash', (req, res) => {
-    const { hash } = req.params;
-    res.end(hash)
+streamer.use('/node/:hash', (stream, headers) => {
+    const { hash } = headers.params;
+    console.log({ hash })
+    stream.end(hash)
 })
 
 const server = http2.createSecureServer({
     key: fs.readFileSync(__dirname + '/localhost-privkey.pem'),
     cert: fs.readFileSync(__dirname + '/localhost-cert.pem'),
 }, app);
+
+server.on('stream', streamer);
 
 server.listen(8443);
