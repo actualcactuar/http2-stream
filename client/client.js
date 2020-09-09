2813e1b01f500f71628f103b834906fe8999baea
jiri <kiviniemi.jiri@gmail.com>
2020-09-09 10:05:16 +0300

    text stream -> videoloopback

--- client/client.js
+++ client/client.js
@@ -1,45 +1,90 @@
-const input = document.getElementById('input');
-const output = document.getElementById('output');
 const close = document.getElementById('close');
 const queryParams = new URLSearchParams(location.search)
+const start = document.getElementById('start');
+const streamview = document.getElementById('streamview');
 const token = queryParams.has('token') ? queryParams.get('token') : (Math.random() * 100).toString(32).replace('.', '')
 
-const stream = new ReadableStream({
-    start(controller) {
-        input.addEventListener('input', (event) => {
-            event.preventDefault();
-            controller.enqueue(input.value);
-            input.value = ""
-        });
+const broadcastSrc = new MediaSource();
+const broadcastUrl = URL.createObjectURL(broadcastSrc);
+streamview.src = broadcastUrl;
+let sourceBuffer;
 
-        close.addEventListener('click', () => controller.close());
-    }
-}).pipeThrough(new TextEncoderStream());
+let updated = true;
+let chunks = [];
+
+broadcastSrc.addEventListener("sourceopen", () => {
+    sourceBuffer = broadcastSrc.addSourceBuffer('video/webm;codecs="vp9"');
+    sourceBuffer.onupdateend = async () => {
+
+        if (chunks.length) {
+            const buffer = Uint8Array.from(...chunks);
+            console.log({ chunks, buffer })
+            chunks = []
+            try {
+                sourceBuffer.appendBuffer(buffer);
+            } catch (err) {
+                console.warn(err)
+            }
+            updated = true;
+        } else {
+            updated = true;
+        }
+    };
+});
 
 
-// Write to server
-fetch('/node/'.concat(token), {
-    method: 'POST', body: stream,
-    headers: { 'Content-Type': 'application/octet-stream' },
-    body: stream,
-})
+const sendMedia = async () => {
+
+    const media = await navigator.mediaDevices.getUserMedia({ video: true });
+    const recorder = new MediaRecorder(media, { mimeType: 'video/webm;codecs="vp9"' });
+
+    const readable = new ReadableStream({
+        start(controller) {
+            recorder.start(100);
+            recorder.ondataavailable = async ({ data }) => {
+                const buffer = await data.arrayBuffer()
+                const uint8 = new Uint8Array(buffer)
+                controller.enqueue(uint8);
+            }
+
+            close.addEventListener('click', () => {
+                recorder.stop();
+                controller.close()
+                for (const track of media.getTracks()) {
+                    track.stop();
+                }
+            });
+        }
+    })
+
+    // Write to server
+    fetch('/node/'.concat(token), {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/octet-stream' },
+        body: readable,
+    })
+}
+
+start.onclick = sendMedia
 
 // read from server
 fetch('/node/'.concat(token),
-    { headers: { 'Content-Type': 'application/octet-stream' } }).then(res => {
+    { headers: { 'Content-Type': 'application/octet-stream' } }).then(async res => {
 
-        const reader = res.body.pipeThrough(new TextDecoderStream()).getReader()
-
-        reader.read().then(function read({ done, value }) {
-            console.log({ value, done })
-
+        const reader = res.body.getReader()
+        while (true) {
+            const { done, value } = await reader.read();
             if (done) {
-                return;
+                broadcastSrc.endOfStream();
+                break;
             }
 
-            output.insertAdjacentText("beforeend", value)
-
-            reader.read().then(read);
-        })
+            if (!updated) {
+                chunks.push(value);
+            } else {
+                sourceBuffer.appendBuffer(value);
+                updated = false;
+            }
+        }
     })
 
